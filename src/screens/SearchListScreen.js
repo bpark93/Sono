3a177dca8271bffc822e433d9bff084992ffe372
Brian Park <bpark12@hotmail.com>
2021-04-20 12:27:23 -0400

    Revised List Screen

--- src/screens/SearchListScreen.js
+++ src/screens/SearchListScreen.js
@@ -14,8 +14,8 @@ import { List, Avatar, ActivityIndicator } from "react-native-paper";
 import { categoryDatabase } from "../../database";
 import { colorPicker } from "../components/RecentPages";
 
-const SearchTutorialListScreen = ({route}) => {
-  const {listId} = route.params
+const SearchTutorialListScreen = ({ route }) => {
+  const { listId } = route.params;
   const navigation = useNavigation();
   const [list, setList] = useState([]);
   useEffect(() => {
@@ -34,82 +34,78 @@ const SearchTutorialListScreen = ({route}) => {
 
   return (
     <ScrollView style={styles.container}>
-      <Text style={{marginHorizontal:15, fontSize:32, fontWeight:'bold', fontFamily:Platform.OS === "android" ? "Roboto-Regular" : null, marginBottom:15}}>{listId === "rapidreviews" ? "Tutorials" : "Tools"}</Text>
-      {list
-        ? list.map((item) => (
-            item.popular ?
+      <Text
+        style={{
+          marginHorizontal: 15,
+          fontSize: 32,
+          fontWeight: "bold",
+          fontFamily: Platform.OS === "android" ? "Roboto-Regular" : null,
+          marginBottom: 15,
+        }}
+      >
+        {listId === "rapidreviews" ? "Tutorials" : "Tools"}
+      </Text>
+      {list ? (
+        list.map((item) =>
+          item.id != "null" && !item.popular ? (
             <List.Item
               key={item.id}
               style={styles.listItemStyle}
               title={item.title}
+              titleStyle={{ fontSize: 18 }}
               description={item.subcategory}
               left={() => (
                 <Avatar.Image
-                  source={categoryDatabase[item.category]}
+                  source={
+                    item.icon
+                      ? { uri: item.icon }
+                      : categoryDatabase[item.category]
+                  }
                   style={{
                     backgroundColor: colorPicker(item.category),
-                    height: 50,
-                    width: 50,
+                    height: 75,
+                    width: 75,
                     justifyContent: "center",
                     alignItems: "center",
                   }}
-                  size={40}
+                  size={55}
                 />
               )}
               onPress={() =>
                 navigation.navigate("SearchDetail", { id: item.id })
               }
-            />:null)): <ActivityIndicator animating={true} size="large"/>}
-      {list
-        ? list.map((item) => (
-            item.id != "null" && !item.popular ?
-            <List.Item
-              key={item.id}
-              style={styles.listItemStyle}
-              title={item.title}
-              description={item.subcategory}
-              left={() => (
-                <Avatar.Image
-                  source={categoryDatabase[item.category]}
-                  style={{
-                    backgroundColor: colorPicker(item.category),
-                    height: 50,
-                    width: 50,
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                  size={40}
-                />
-              )}
-              onPress={() =>
-                navigation.navigate("SearchDetail", { id: item.id })
-              }
-            /> :
+            />
+          ) : (
             <List.Item
               key={item.title}
               style={styles.listItemStyle}
               title={`${item.title} - Coming Soon`}
-              titleStyle={{color:'#D0D0D0'}}
+              titleStyle={{ color: "#D0D0D0", fontSize: 18 }}
               description={item.subcategory}
-              descriptionStyle={{color:"#D0D0D0"}}
+              descriptionStyle={{ color: "#D0D0D0" }}
               left={() => (
                 <Avatar.Image
-                  source={categoryDatabase[item.category]}
+                  source={
+                    item.icon
+                      ? { uri: item.icon }
+                      : categoryDatabase[item.category]
+                  }
                   style={{
                     backgroundColor: colorPicker(item.category),
-                    height: 50,
-                    width: 50,
+                    height: 75,
+                    width: 75,
                     justifyContent: "center",
                     alignItems: "center",
                   }}
-                  size={40}
+                  size={55}
                 />
               )}
             />
-          ))
-        : (
-          <Text>No List</Text>
-        )}
+          )
+        )
+      ) : (
+        <ActivityIndicator size="large" animating={true} />
+      )}
     </ScrollView>
   );
 };
@@ -118,7 +114,7 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#ffffff",
     flex: 1,
-    paddingVertical:10,
+    paddingVertical: 10,
   },
   listItemStyle: {
     marginHorizontal: 5,
